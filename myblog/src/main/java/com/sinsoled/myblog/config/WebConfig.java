cc9a407d476516f775771df00f136a4968f4cb8f
sinsoled <1797719643@qq.com>
2021-03-20 01:03:51 +0800

    SpringSecurity

--- myblog/src/main/java/com/sinsoled/myblog/config/WebConfig.java
+++ myblog/src/main/java/com/sinsoled/myblog/config/WebConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -27,4 +28,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
 
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    }
+
 }
--- myblog/src/main/java/com/sinsoled/myblog/security/handler/MyAuthFailureHandler.java
+++ myblog/src/main/java/com/sinsoled/myblog/security/handler/MyAuthFailureHandler.java
@@ -1,7 +1,9 @@
 package com.sinsoled.myblog.security.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sinsoled.myblog.utils.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -18,14 +20,28 @@ import java.io.IOException;
  * @date 2021年3月20日00:19:01
  */
 @Configuration
+@Slf4j
 public class MyAuthFailureHandler implements AuthenticationFailureHandler {
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-
+        String exName = exception.getClass().getSimpleName();
+        switch (exName) {
+            case "AccountExpiredException":
+                response.getWriter().write(JSON.toJSONString(ResultUtil.failure("accountExpired")));
+                break;
+            case "UsernameNotFoundException":
+                response.getWriter().write(JSON.toJSONString(ResultUtil.failure("usernameNotFound")));
+                break;
+            case "BadCredentialsException":
+                response.getWriter().write(JSONObject.toJSONString(ResultUtil.failure("passwordError")));
+                break;
+            case "LockedException":
+                response.getWriter().write(JSONObject.toJSONString(ResultUtil.failure("disabledAccount")));
+                break;
+            default:
+                log.trace("exName" + exName);
+        }
     }
 
 }
