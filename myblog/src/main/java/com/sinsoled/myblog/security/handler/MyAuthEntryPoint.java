26747aae4e1a5784bed9f40215eb66099c8f15b1
sinsoled <1797719643@qq.com>
2021-03-20 12:19:39 +0800

    SpringSecurity

--- myblog/src/main/java/com/sinsoled/myblog/security/handler/MyAuthEntryPoint.java
+++ myblog/src/main/java/com/sinsoled/myblog/security/handler/MyAuthEntryPoint.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,7 +18,7 @@ import java.io.IOException;
 public class MyAuthEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.getWriter().write(JSON.toJSONString(ResultUtil.failure("登录超时, 请重新登录!")));
     }
 }
